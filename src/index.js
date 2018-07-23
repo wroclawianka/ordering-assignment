3102e77fb735f1b9d92232a0d0ab4bae0d994f68
wroclawianka <dorota.zelga@gmail.com>
2018-07-23 20:55:08 +0200

    cleaning after app generation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <p>Hello</p>
+        </div>
+      );
+    }
+  }
+  
+render(<App/>, window.document.getElementById("app"))
