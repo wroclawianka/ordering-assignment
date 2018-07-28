3ae7a1b53c1eadab5b048de72090fb1d5782cbe9
wroclawianka <dorota.zelga@gmail.com>
2018-07-28 11:45:30 +0200

    Item model added to AdditionalProduct

---
+++ src/app/components/AdditionalProductsList/AdditionalProduct/Models/Item.js
@@ -0,0 +1,8 @@
+export class Item {
+  constructor (productId, quantity, unitPrice, total) {
+    this.productId = productId
+    this.quantity = quantity
+    this.unitPrice = unitPrice
+    this.total = total
+  }
+}
